a4a3f32318981936b7cf7898884e0aca5dfec5b4
Gregor Laan <gregor@github.ee>
2018-05-23 23:32:54 +0300

    Display social media base url

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
-        websiteUrl: 'www.yourWebsite.com'
+        websiteUrl: 'www.yourWebsite.com',
+        facebookBaseUrl: 'https://www.facebook.com/sharer.php?u=',
+        googleBaseUrl: 'https://plus.google.com/share?url=',
+        twitterBaseUrl: 'https://twitter.com/share?url=',
+        linkedinBaseUrl: 'https://www.linkedin.com/shareArticle?mini=true&url='
     }
 });
