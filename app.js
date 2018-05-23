2835324e05eefddacfea51e50d99e5fadaf630a0
Gregor Laan <gregor@github.ee>
2018-05-23 23:39:51 +0300

    Every site url should start with https protocol

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var app = new Vue({
     el: '#app',
     data: {
         websiteUrl: 'www.yourWebsite.com',
-        facebookBaseUrl: 'https://www.facebook.com/sharer.php?u=',
-        googleBaseUrl: 'https://plus.google.com/share?url=',
-        twitterBaseUrl: 'https://twitter.com/share?url=',
-        linkedinBaseUrl: 'https://www.linkedin.com/shareArticle?mini=true&url='
+        facebookBaseUrl: 'https://www.facebook.com/sharer.php?u=https://',
+        googleBaseUrl: 'https://plus.google.com/share?url=https://',
+        twitterBaseUrl: 'https://twitter.com/share?url=https://',
+        linkedinBaseUrl: 'https://www.linkedin.com/shareArticle?mini=true&url=https://'
     }
 });
