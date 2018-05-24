95815cc96b18a66f6bb2fca7d3df08fc9b2b870b
Gregor Laan <gregor@github.ee>
2018-05-24 12:46:33 +0300

    Display name inside a button

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ var app = new Vue({
     data: {
         websiteUrl: 'www.yourWebsite.com',
         baseUrl: [
-            { name: 'facebook', url: 'https://www.facebook.com/sharer.php?u=https://' },
-            { name: 'google', url: 'https://plus.google.com/share?url=https://' },
-            { name: 'twitter', url: 'https://twitter.com/share?url=https://' },
-            { name: 'linkedin', url: 'https://www.linkedin.com/shareArticle?mini=true&url=https://' }
+            { name: 'Facebook', url: 'https://www.facebook.com/sharer.php?u=https://' },
+            { name: 'Google+', url: 'https://plus.google.com/share?url=https://' },
+            { name: 'Twitter', url: 'https://twitter.com/share?url=https://' },
+            { name: 'Linkedin', url: 'https://www.linkedin.com/shareArticle?mini=true&url=https://' }
         ]
     },
     methods: {
-        displayButton: function (baseUrl) {
-            var button = '<a href="' + baseUrl + this.websiteUrl + '"></a>';
+        displayButton: function (baseUrl, baseName) {
+            var button = '<a href="' + baseUrl + this.websiteUrl + '">' + baseName +'</a>';
             return button;
         }
     }
